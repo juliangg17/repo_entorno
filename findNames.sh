cec97f6eaab1dfa938675ae99228936b81c3f031
Julian Garcia <juliangg17@gmail.com>
2022-07-17 22:55:05 -0300

    Se mejora el código de findNames.sh y se comenta dentro.

--- findNames.sh
+++ findNames.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-while read line; do
+#################################################################################################################################################
+#Identificación de nombres propios (se identifican sólo si están en este formato Nnnnnnnnn), aunque la palabra no sea un nombre propio realmente.
+#Ejemplos: Mateo, Estonoesunnombre, Ana.
+################################################################################################################################################
+
+cat $1 | sed 's/[^a-zA-Z0-9 ]//g' | tr ' ' '\n' > depurado.txt	#con sed elimina el archivo de caracteres no alfanuméricos
+								#con tr sustituye los espacios por saltos de línea
+								#guarda los cambios en el archivo depurado.txt
+while read line; do						#${name sombrero} devuelve un string en formato Nnnnnnnn
 	name=$(echo -n $line)
 	#echo ${name^}
 	if [ $name = ${name^} ]; then echo $name; fi
-done < $1
+done < depurado.txt
 
 exit 0
