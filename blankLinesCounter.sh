1a82241e64254a28b7fc0ba6d1342aa1b0b9ab5f
Julian Garcia <juliangg17@gmail.com>
2022-07-17 23:00:33 -0300

    Se mejora el código de blankLinesCounter.sh y se comenta dentro.

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -4,7 +4,9 @@
 #Este script cuenta las líneas en blanco de un archivo txt. El argumento $1 es el nombre del archivo.
 #########################################################################################################
 
-grep -cx '' $1 	#La opción x selecciona solamente aqellas líneas donde '' coincide con toda la línea. 
-		#La opción c sirve para devolver la cantidad de líneas que concuerdan con lo que se filtra.
+cat $1 | sed 's/[^a-zA-Z0-9 ]//g' | tr ' ' '\n' > depurado.txt	#limpia el archivo de entrada de caracteres no alfanuméricos,
+								#separa las palabras en líneas y guarda todo en depurado.txt
+grep -cx '' depurado.txt	 	#La opción x selecciona solamente aqellas líneas donde '' coincide con toda la línea. 
+					#La opción c sirve para devolver la cantidad de líneas que concuerdan con lo que se filtra.
 
 exit 0
