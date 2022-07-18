63745d03432882d71e5240cc1a3740e20306d065
Julian Garcia <juliangg17@gmail.com>
2022-07-18 15:34:13 -0300

    Se mejora el código y se agregan comentarios dentro del substringReplace.sh

--- substringReplace.sh
+++ substringReplace.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-cp $3 "copia$3"
 
+################################################################################################################################################
+#Reemplazo de subcadenas, que considere diferencias entre minúsculas y mayúsculas, pero ignore acentos. Recibe dos cadenas, y cada aparición de
+#cadena1 debe reemplazarse por la cadena2.
+#Ejemplo: cadena1: tre cadena2: TRE las palabras: enTrepiso, entretenido, intrépido pasan a: enTrepiso, enTREtenido, inTREpido
+################################################################################################################################################
 
-sed -i "s/á/a/" "copia$3"
+#Este script recibe tres argumentos: $1 el texto a reemplazar, $2 el texto nuevo y $3 el nombre del archivor los reemplazos
 
-sed -i "s/$1/$2/" "copia$3"
-cat "copia$3"
-
+sed 'y/áéíóú/aeiou/' $3 > temp.txt		#elimina los acentos de $3 y guarda los cambios en temp.txt
+sed "s/$1/$2/g" temp.txt 			#reemplaza las cadenas $1 por $2 en el archivo temp.txt
+rm temp.txt
 exit 0
