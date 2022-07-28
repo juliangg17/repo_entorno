20525a5bc8903423efe1ca1e2b3b0e0c4123d34c
Julian Garcia <juliangg17@gmail.com>
2022-07-28 08:12:48 -0300

    agrego variable ARCHIVO

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -17,10 +17,13 @@ case $RESP in
 
         [Ss])
 
-	cat $1 | sed 's/[^a-zA-Z0-9 ]//g' | tr ' ' '\n' > temp.txt	#limpia el archivo de entrada de caracteres no alfanuméricos,
-									#separa las palabras en líneas y guarda todo en temp.txt
-	grep -cx '' temp.txt					 	#La opción x selecciona solamente aqellas líneas donde '' coincide con toda la línea. 
-	rm temp.txt							#La opción c sirve para devolver la cantidad de líneas que concuerdan con lo que se filtra.
+        echo "Ingrese el nombre del archivo que desea procesar:"
+        read ARCHIVO
+
+	cat $ARCHIVO | sed 's/[^a-zA-Z0-9 ]//g' | tr ' ' '\n' > temp.txt	#limpia el archivo de entrada de caracteres no alfanuméricos,
+										#separa las palabras en líneas y guarda todo en temp.txt
+	grep -cx '' temp.txt						 	#La opción x selecciona solamente aqellas líneas donde '' coincide con toda la línea. 
+	rm temp.txt								#La opción c sirve para devolver la cantidad de líneas que concuerdan con lo que se filtra.
 
 	exit 0
 ;;
