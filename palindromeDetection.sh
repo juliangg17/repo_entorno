4e1dfb1e65d4e2113f8e92cc7d2a3142f9561312
Julian Garcia <juliangg17@gmail.com>
2022-07-28 22:32:03 -0300

    Se actualizan palindromo y substring

--- palindromeDetection.sh
+++ palindromeDetection.sh
@@ -4,22 +4,41 @@
 #Mostrar palabras palíndromo (ignorar mayúsculas/minúsculas y acentos en este caso). Ejemplos: Neuquén, radar, reconocer
 #################################################################################################################################################
 
-sed 'y/áéíóú/aeiou/' $1 > temp.txt 			#elimina los acentos
+echo "Este programa muestra las palabras palíndromo (por ej. Nuequén o radar)."
+echo "¿Desea seguir? [S/N]"
+read RESP
 
-cat temp.txt | sed 's/[^a-zA-Z ]//g' | tr [:upper:] [:lower:] | tr ' ' '\n' | tr -s '\n' '\n'> temp2.txt
+case $RESP in
+[Nn])
 
-#arriba elimina los caracteres no alfanuméricos, pasa todo a minúsculas, pone una palabra por línea y borra los saltos de línea vacíos
+        bash Enunciadotp.sh
+        exit 0
+;;
 
-while read line; do
-	NORMAL=$(echo $line)
-	REVERSO=$(echo $line | rev)
-	LONG=$(echo $line | tr -d '\n' | wc -c)
-	if [ "$NORMAL" == "$REVERSO" ] && [ $LONG -gt 1 ]; then echo $line; fi
-done < temp2.txt
+        [Ss])
 
-#arriba compara cada línea (palabra) con su reverso. Las palabras de una letra no se analizan.
+        echo "Ingrese el nombre del archivo que desea procesar:"
+        read ARCHIVO
 
-rm temp.txt
-rm temp2.txt
+	sed 'y/áéíóú/aeiou/' $ARCHIVO > temp.txt 		#elimina los acentos
 
-exit 0
+	cat temp.txt | sed 's/[^a-zA-Z ]//g' | tr [:upper:] [:lower:] | tr ' ' '\n' | tr -s '\n' '\n'> temp2.txt
+
+	#arriba elimina los caracteres no alfanuméricos, pasa todo a minúsculas, pone una palabra por línea y borra los saltos de línea vacíos
+
+	echo 'Las sguientes palabras son palíndromos:'
+	while read line; do
+		NORMAL=$(echo $line)
+		REVERSO=$(echo $line | rev)
+		LONG=$(echo $line | tr -d '\n' | wc -c)
+		if [ "$NORMAL" == "$REVERSO" ] && [ $LONG -gt 1 ]; then echo $line; fi
+	done < temp2.txt
+
+	#arriba compara cada línea (palabra) con su reverso. Las palabras de una letra no se analizan.
+
+	rm temp.txt
+	rm temp2.txt
+
+	exit 0
+;;
+esac
--- substringReplace.sh
+++ substringReplace.sh
@@ -8,7 +8,32 @@
 
 #Este script recibe tres argumentos: $1 el nombre del archivo, $2 el texto original a reemplazar y $3 el texto nuevo
 
-sed 'y/áéíóú/aeiou/' $1 > temp.txt		#elimina los acentos de $1 y guarda los cambios en temp.txt
-sed "s/$2/$3/g" temp.txt 			#reemplaza las cadenas $2 por $3 en el archivo temp.txt
-rm temp.txt
-exit 0
+echo "Este programa reemplaza una cadena de texto por otra dentro de un archivo."
+echo "¿Desea seguir? [S/N]"
+read RESP
+
+case $RESP in
+[Nn])
+
+        bash Enunciadotp.sh
+        exit 0
+;;
+
+        [Ss])
+
+        echo "Ingrese el nombre del archivo que desea procesar:"
+        read ARCHIVO
+
+        echo "Ingrese la cadena de texto a ser reemplazada:"
+        read TXTORIG
+
+        echo "Ingrese la nueva cadena de texto:"
+        read TXTNUE
+
+	sed 'y/áéíóú/aeiou/' $ARCHIVO > temp.txt	#elimina los acentos de $1 y guarda los cambios en temp.txt
+	sed "s/$TXTORIG/$TXTNUE/g" temp.txt 			#reemplaza las cadenas $2 por $3 en el archivo temp.txt
+	rm temp.txt
+	exit 0
+
+;;
+esac
