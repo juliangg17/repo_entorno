c1a67e5e68b964a7d84769b9304f47eb246e5e1e
Ignacio Isaurralde <iisaurralde13@gmail.com>
2022-07-30 17:06:54 -0300

    Agrego script al Enunciado

--- mailAddressDetection.sh
+++ mailAddressDetection.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
-#Detectar y listar las direcciones de mail encontradas dentro del texto,
-#ordenarlas y evitar repeticiones. Ejemplo: nombre@dominio.com
-#Ayuda: utilizar una expresión regular.
-
-#agregar limpieza de archivo 
-
-regex="^[a-z\._0-9]+@[a-z]+\.[a-z]{2,5}\.?[a-z]{2,5}?$"
-
-touch temp_words.txt
+######################################################################################################################################
+# Detectar y listar las direcciones de mail encontradas dentro del texto, ordenarlas y evitar repeticiones.
+# Ejemplo: nombre@dominio.com.  Ayuda: utilizar una expresión regular.
+#####################################################################################################################################
 
 
-while read line
-do
-	if [[ $line =~ $regex ]]
-	then
-		echo $line >> temp_words.txt
-	fi
-done < $1
+echo "Este programa devuelve las direcciones de mail que se encuentran dentro de un texto, ordenadas y sin repetir"
+echo "¿Desea seguir? [S/N]"
+echo
+read RESP
 
-sort temp_words.txt | uniq
+case $RESP in
+[Nn])
 
-rm -f temp_words.txt
+        bash Enunciadotp.sh
+        exit 0
+;;
 
-exit 0
+        [Ss])
+
+        echo "Ingrese el nombre del archivo que desea procesar:"
+        read ARCHIVO
+
+        sed 'y/áéíóú/aeiou/' $ARCHIVO > temp.txt                #elimina los acentos
+
+        cat temp.txt | tr [:upper:] [:lower:] | tr ' ' '\n' | tr -s '\n' '\n' > temp2.txt
+
+        #arriba pasa todo a minúsculas, pone una palabra por línea y borra los saltos de línea vacíos
+
+
+	regex="^[a-z\._0-9]+@[a-z]+\.[a-z]{2,5}\.?[a-z]{2,5}?$"  # Expresion regular para encontrar correos electrónicos
+	> temp.txt
+
+	while read line
+	do
+		if [[ $line =~ $regex ]]
+		then
+			echo $line >> temp.txt
+		fi
+	done < temp2.txt
+
+	sort temp.txt | uniq
+
+	rm -f temp.txt temp2.txt
+
+	exit 0
+;;
+esac
