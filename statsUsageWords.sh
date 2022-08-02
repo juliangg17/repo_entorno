26d3bd82eb62e99253d88e9de1ee5c185b65b6c8
Francisca Herrera <francisca-herrera@outlook.es>
2022-08-02 00:45:33 -0300

    Subo ej actulizado

--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-#ver acentos
+################################################################################################################################################
+#Indicador estadístico de uso de palabras, deben ser de al menos 4(cuatro) letras.
+#Mostrar un Top Ten de estas palabras ordenadas desde la que tiene más apariciones a la que tiene menos).
+#Es decir, filtrar las palabras que tengan al menos 4 letras y de éstas, elegir las 10 más usadas.
+#################################################################################################################################################
 
-cat $1 | tr \  '\n' | tr -c -d '[:alpha:]\n' | tr '[:upper:]' '[:lower:]' | grep ^.... | sort | uniq -c | sort -nr | head -10
+echo "Este programa filtra palabras de 4 letras o más, y de éstas selecciona las 10 más usadas"
+
+echo "¿Desea seguir? [S/N]"
+read RESP
+
+case $RESP in
+[Nn])
+
+        bash Enunciadotp.sh
+        exit 0
+;;
+
+        [Ss])
+
+        echo "Ingrese el nombre del archivo que desea procesar:"
+        read ARCHIVO
+
+sed 'y/áéíóú/aeiou/' $ARCHIVO > temp.txt	#elimina los acentos del archivo y guarda los cambios en temp.txt
+
+cat temp.txt | tr \  '\n' | tr -c -d '[:alpha:]\n' | tr '[:upper:]' '[:lower:]' | grep ^.... | sort | uniq -c | sort -nr | head -10
+
+rm temp.txt
+
+exit 0
+
+;;
+esac
 
 
 
